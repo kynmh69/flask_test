6e24405e70c56d3f194e0c2c1ffb588740c2ebf2
kynmh69 <hiroki.kayanuma@gmail.com>
2020-07-24 15:52:58 +0900

    add index page.

--- hello.py
+++ hello.py
@@ -1,10 +1,27 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path, endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+
 @app.route('/')
 def index():
-    return 'index'
+    return render_template('index.html')
+
 
 @app.route('/hello')
 def hello():
@@ -12,4 +29,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
