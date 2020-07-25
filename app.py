8a7607c183b453df04f2f0a9fb3c8e484394a4a0
kynmh69 <hiroki.kayanuma@gmail.com>
2020-07-25 17:21:43 +0900

    add fire base

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import os
+from typing import List
 from flask import Flask, render_template, url_for
 import firebase_admin
+from google.cloud.firestore_v1.client import Client
+from google.cloud.firestore_v1.document import DocumentSnapshot
 from firebase_admin.credentials import Certificate
 from firebase_admin import firestore
+from firebase_admin.db import Query
 
 PROJECT_ID = 'mywebpage-3a2bd'
 SERVICE_ACCOUNT_KEY_PATH = '/Users/Hiroki/Applications/PythonProjects/flask_test/mywebpage-3a2bd-firebase-adminsdk-o3ntg-8a383c01f5.json'
@@ -10,11 +14,10 @@ app = Flask(__name__)
 cred = Certificate(SERVICE_ACCOUNT_KEY_PATH)
 firebase_admin.initialize_app(cred, dict(projectId=PROJECT_ID))
 
-db = firestore.client()
+db: Client = firestore.client()
 users_ref = db.collection(u'users')
-docs = users_ref.steam()
-for doc in docs:
-    print(u'{} => {}'.format(doc.id, doc.to_dict()))
+docs: Query = users_ref.start_at(dict(username='hiroki'))
+print(docs.get())
 
 
 @app.context_processor
@@ -37,8 +40,44 @@ def index():
 
 
 @app.route('/login')
-def hello():
+def login():
     return render_template('login.html')
+
+
+def regist_user(user_name: str, password: str) -> None:
+    return
+
+
+def find_by_user(username: str):
+    return
+
+
+class User(object):
+    def __init__(self, user_name: str, email: str):
+        """
+        """
+        super().__init__()
+        self.__user_name = user_name
+        self.__email = email
+        return
+
+    @property
+    def user_name(self) -> str:
+        return self.__user_name
+
+    @user_name.setter
+    def user_name(self, user_name) -> None:
+        self.__user_name = user_name
+        return
+
+    @property
+    def email_address(self) -> str:
+        return self.__email
+
+    @email_address.setter
+    def email_address(self, email) -> None:
+        self.__email = email
+        return
 
 
 if __name__ == "__main__":
